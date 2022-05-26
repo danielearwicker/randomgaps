aafac7790d97f246a2774c549ec6503654da1188
Daniel Earwicker <dearwicker@fiscaltec.com>
2022-05-26 11:22:56 +0100

    FIRST

---
+++ dist/index.242b51c6.js
@@ -0,0 +1,54 @@
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = canvas.clientWidth * window.devicePixelRatio;
+canvas.height = canvas.clientHeight * window.devicePixelRatio;
+ctx.strokeStyle = "purple";
+ctx.lineCap = "round";
+ctx.lineWidth = 16;
+function arc(from, to) {
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height * 0.4, Math.PI * (from + 1), Math.PI * (to + 1), false);
+    ctx.stroke();
+}
+var pieces = [
+    [
+        0,
+        0.5
+    ],
+    [
+        0.55,
+        0.65
+    ],
+    [
+        0.7,
+        0.8
+    ],
+    [
+        0.85,
+        1
+    ]
+];
+function render(value1) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (const [from, to] of pieces){
+        ctx.globalAlpha = 0.3;
+        arc(from, to);
+        ctx.globalAlpha = 1;
+        if (value1 > to) arc(from, to);
+        else if (value1 > from) arc(from, value1);
+    }
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = "24pt Helvetica";
+    ctx.fillText("random gaps!", canvas.width / 2, canvas.height / 2);
+}
+let value = 0;
+function doFrame() {
+    render(value);
+    value += 0.01;
+    if (value > 1) value = 0;
+    requestAnimationFrame(doFrame);
+}
+doFrame();
+
+//# sourceMappingURL=index.242b51c6.js.map
